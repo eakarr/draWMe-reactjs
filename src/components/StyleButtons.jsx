9f26d85ffa6dd5fb4ae171319fe11b8c5ea7f148
eakarr <eaakar.55@gmail.com>
2022-06-04 13:01:19 +0300

    Jsx modified

--- src/components/StyleButtons.jsx
+++ src/components/StyleButtons.jsx
@@ -2,15 +2,14 @@ import React from "react";
 
 import "./StyleButtons.scss";
 
-const StyleButtons = ({ id, onClick, src }) => {
+const StyleButtons = ({ id, onClick, src, toolType }) => {
   return (
-    <div className="icon-container">
-      <img
-        src={src}
-        id={id}
-        alt={id}
-        onClick={onClick}
-      />
+    <div
+      onClick={onClick}
+      id={id}
+      className={toolType === id ? "icon-container active" : "icon-container"}
+    >
+      <img src={src} alt={id} />
     </div>
   );
 };
