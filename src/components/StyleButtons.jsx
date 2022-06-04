be8d152a4b95d1ce64cd9f32d3a66639217a2eff
eakarr <eaakar.55@gmail.com>
2022-06-05 00:11:52 +0300

    Div tag changed into button tag

--- src/components/StyleButtons.jsx
+++ src/components/StyleButtons.jsx
@@ -4,13 +4,13 @@ import "./StyleButtons.scss";
 
 const StyleButtons = ({ id, onClick, src, toolType }) => {
   return (
-    <div
+    <button
       onClick={onClick}
       id={id}
       className={toolType === id ? "icon-container active" : "icon-container"}
     >
       <img src={src} alt={id} />
-    </div>
+    </button>
   );
 };
 
