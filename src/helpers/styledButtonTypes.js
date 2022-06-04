acfd5ed1a35fb2b488a20d2a6c2a5b04eef3fab4
eakarr <eaakar.55@gmail.com>
2022-06-04 13:24:23 +0300

    Initial commit

---
+++ src/helpers/styledButtonTypes.js
@@ -0,0 +1,11 @@
+import mouseIcon from "../assets/mouse-icon.svg";
+import squareIcon from "../assets/square-icon.svg";
+import lineIcon from "../assets/line-icon.svg";
+
+const styledButtonTypes = [
+  { id: "selection", icon: mouseIcon },
+  { id: "line", icon: lineIcon },
+  { id: "rectangle", icon: squareIcon },
+];
+
+export default styledButtonTypes;
