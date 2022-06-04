512e996a2763db3883e5bf8ffc86b2ddbf5dd12e
eakarr <eaakar.55@gmail.com>
2022-06-05 00:12:35 +0300

    Initial commit

---
+++ src/hooks/useHistory.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+// This hook is for to keep the snapshots of the drawings so that we can use redo-undo logic.
+export const useHistory = (initialState) => {
+  const [index, setIndex] = useState(0);
+  const [history, setHistory] = useState([initialState]);
+
+  const setState = (action, overwrite = false) => {
+    const newState =
+      typeof action === "function" ? action(history[index]) : action;
+    if (overwrite) {
+      const historyCopy = [...history];
+      historyCopy[index] = newState;
+      setHistory(historyCopy);
+    } else {
+      const updatedState = [...history].slice(0, index + 1); // This is for cleaning up the state snapshots' indexes after undo-redo process. If we draw something and undo it and then draw something new, the previous indexes and snapshots should be deleted.
+      setHistory([...updatedState, newState]);
+      setIndex((prevState) => prevState + 1);
+    }
+  };
+
+  const undo = () => index > 0 && setIndex((prevState) => prevState - 1);
+  const redo = () =>
+    index < history.length - 1 && setIndex((prevState) => prevState + 1);
+
+  return [history[index], setState, undo, redo];
+};
