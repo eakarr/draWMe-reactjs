b64a9c46e455b5ca672539a8cede38ee40aa41ab
eakarr <eaakar.55@gmail.com>
2022-06-06 20:14:53 +0300

    Codes are rearranged

--- src/helpers/adjustElementCoordinates.js
+++ src/helpers/adjustElementCoordinates.js
@@ -1,19 +1,19 @@
 // adjustElementCoordinates function makes us sure that if we start drawing from a point towards to the left side of the drawing, when we stopped drawing, the left top side of the drawing will be x1,y1. Not x2,y2.
 const adjustElementCoordinates = (element) => {
-    const { type, x1, y1, x2, y2 } = element;
-    if (type === "rectangle") {
-      const minX = Math.min(x1, x2);
-      const maxX = Math.max(x1, x2);
-      const minY = Math.min(y1, y2);
-      const maxY = Math.max(y1, y2);
-      return { x1: minX, y1: minY, x2: maxX, y2: maxY };
+  const { type, x1, y1, x2, y2 } = element;
+  if (type === "rectangle") {
+    const minX = Math.min(x1, x2);
+    const maxX = Math.max(x1, x2);
+    const minY = Math.min(y1, y2);
+    const maxY = Math.max(y1, y2);
+    return { x1: minX, y1: minY, x2: maxX, y2: maxY };
+  } else {
+    if (x1 < x2 || (x1 === x2 && y1 < y2)) {
+      return { x1, y1, x2, y2 };
     } else {
-      if (x1 < x2 || (x1 === x2 && y1 < y2)) {
-        return { x1, y1, x2, y2 };
-      } else {
-        return { x1: x2, y1: y2, x2: x1, y2: y1 };
-      }
+      return { x1: x2, y1: y2, x2: x1, y2: y1 };
     }
-  };
+  }
+};
 
-  export default adjustElementCoordinates;
+export default adjustElementCoordinates;
