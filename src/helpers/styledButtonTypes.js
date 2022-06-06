9738e1e966e294a120cc612b0693a7c162aff570
eakarr <eaakar.55@gmail.com>
2022-06-06 17:32:24 +0300

    Pencil type added

--- src/helpers/styledButtonTypes.js
+++ src/helpers/styledButtonTypes.js
@@ -1,11 +1,13 @@
 import mouseIcon from "../assets/mouse-icon.svg";
 import squareIcon from "../assets/square-icon.svg";
 import lineIcon from "../assets/line-icon.svg";
+import pencilIcon from "../assets/pencil-icon.svg";
 
 const styledButtonTypes = [
   { id: "selection", icon: mouseIcon },
   { id: "line", icon: lineIcon },
   { id: "rectangle", icon: squareIcon },
+  { id: "pencil", icon: pencilIcon },
 ];
 
 export default styledButtonTypes;
