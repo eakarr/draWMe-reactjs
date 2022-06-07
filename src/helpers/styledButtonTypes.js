1bd0602ca8bde6cec5b54ff40082f07ca3c0c6d8
eakarr <eaakar.55@gmail.com>
2022-06-07 14:35:54 +0300

    Text type added

--- src/helpers/styledButtonTypes.js
+++ src/helpers/styledButtonTypes.js
@@ -2,12 +2,14 @@ import mouseIcon from "../assets/mouse-icon.svg";
 import squareIcon from "../assets/square-icon.svg";
 import lineIcon from "../assets/line-icon.svg";
 import pencilIcon from "../assets/pencil-icon.svg";
+import textIcon from "../assets/text-icon.svg";
 
 const styledButtonTypes = [
   { id: "selection", icon: mouseIcon },
   { id: "line", icon: lineIcon },
   { id: "rectangle", icon: squareIcon },
   { id: "pencil", icon: pencilIcon },
+  { id: "text", icon: textIcon },
 ];
 
 export default styledButtonTypes;
