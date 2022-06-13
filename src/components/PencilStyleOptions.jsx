23cbf21d6154431401035c33f6c80a0a2eb36576
eakarr <eaakar.55@gmail.com>
2022-06-13 16:14:20 +0300

    Initial commit

---
+++ src/components/PencilStyleOptions.jsx
@@ -0,0 +1,93 @@
+import { useContext } from "react";
+import { StyleOptionsContext } from "../context/StyleOptionsContext";
+
+import "./PencilStyleOptions.scss";
+
+const PencilStyleOptions = () => {
+  const {
+    pencilSize,
+    setPencilSize,
+    pencilThinning,
+    setPencilThinning,
+    pencilStreamline,
+    setPencilStreamline,
+    pencilSmoothing,
+    setPencilSmoothing,
+  } = useContext(StyleOptionsContext);
+
+  const pencilStylingFeatures = [
+    {
+      id: "Size",
+      labelName: "Size",
+      featureType: pencilSize,
+      setFeatureType: setPencilSize,
+    },
+    {
+      id: "Thinning",
+      labelName: "Thinning",
+      featureType: pencilThinning,
+      setFeatureType: setPencilThinning,
+    },
+    {
+      id: "Streamline",
+      labelName: "Streamline",
+      featureType: pencilStreamline,
+      setFeatureType: setPencilStreamline,
+    },
+    {
+      id: "Smoothing",
+      labelName: "Smoothing",
+      featureType: pencilSmoothing,
+      setFeatureType: setPencilSmoothing,
+    },
+  ];
+
+  const increaseSizeButtonHandler = (pencilStyleType, setPencilStyleType) => {
+    if (pencilStyleType <= 0.98) {
+      setPencilStyleType((prevState) => prevState + 0.02);
+    }
+    return;
+  };
+
+  const decreaseSizeButtonHandler = (pencilStyleType, setPencilStyleType) => {
+    if (pencilStyleType >= 0.04) {
+      setPencilStyleType((prevState) => prevState - 0.02);
+    }
+    return;
+  };
+
+  return (
+    <div className="pencil-style-options-container">
+      {pencilStylingFeatures.map((style) => (
+        <div className="pencil-style-options" key={Math.random()}>
+          <label htmlFor="Size">{style.labelName}</label>
+          <div className="pencil-style-option-button-container">
+            <button
+              onClick={() =>
+                increaseSizeButtonHandler(
+                  style.featureType,
+                  style.setFeatureType
+                )
+              }
+            >
+              +
+            </button>
+            <span>{(style.featureType).toFixed(2)}</span>
+            <button
+              onClick={() =>
+                decreaseSizeButtonHandler(
+                  style.featureType,
+                  style.setFeatureType
+                )
+              }
+            >
+              -
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PencilStyleOptions;
