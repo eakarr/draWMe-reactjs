f6844adac66c03633495a0e9c94f351325816afa
eakarr <eaakar.55@gmail.com>
2022-06-13 16:14:56 +0300

    Initial commit

---
+++ src/context/StyleOptionsContext.jsx
@@ -0,0 +1,27 @@
+import { useState, createContext } from "react";
+
+export const StyleOptionsContext = createContext();
+
+export const StyleOptionsProvider = ({ children }) => {
+  const [pencilSize, setPencilSize] = useState(0.16);
+  const [pencilThinning, setPencilThinning] = useState(0.5);
+  const [pencilStreamline, setPencilStreamline] = useState(0.5);
+  const [pencilSmoothing, setPencilSmoothing] = useState(0.5);
+
+  return (
+    <StyleOptionsContext.Provider
+      value={{
+        pencilSize,
+        setPencilSize,
+        pencilThinning,
+        setPencilThinning,
+        pencilStreamline,
+        setPencilStreamline,
+        pencilSmoothing,
+        setPencilSmoothing,
+      }}
+    >
+      {children}
+    </StyleOptionsContext.Provider>
+  );
+};
