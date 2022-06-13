42cdd45e65fc4a157103eea37d8ab69c686eb0e0
eakarr <eaakar.55@gmail.com>
2022-06-13 16:14:37 +0300

    Initial commit

---
+++ src/components/ToolButtons.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./ToolButtons.scss";
+
+const StyleButtons = ({ id, onClick, src, toolType }) => {
+  return (
+    <button
+      onClick={onClick}
+      id={id}
+      className={toolType === id ? "icon-container active" : "icon-container"}
+    >
+      <img src={src} alt={id} />
+    </button>
+  );
+};
+
+export default StyleButtons;
