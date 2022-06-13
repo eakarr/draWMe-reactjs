c6ca3dd8a624802dddf0914a5a5cb803201deece
eakarr <eaakar.55@gmail.com>
2022-06-13 16:13:39 +0300

    Toggle feature added

--- src/components/BurgerButton.jsx
+++ src/components/BurgerButton.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import PencilStyleOptions from "./PencilStyleOptions"
 
 import burgerIcon from "../assets/burger-icon.svg";
 import "./BurgerButton.scss";
 
 const BurgerButton = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleHandler = () => {
+    setToggle((prevState) => !prevState);
+  };
+
   return (
-    <div className="container-burger-button">
-      <div className="icon-container">
-        <img src={burgerIcon} alt="burger" />
+    <>
+      <div className="container-burger-button">
+        <div className={toggle ? "burger active" : "burger"} onClick={toggleHandler}>
+          <img src={burgerIcon} alt="burger" />
+        </div>
       </div>
-    </div>
+      {toggle ? <PencilStyleOptions /> : null}
+    </>
   );
 };
 
