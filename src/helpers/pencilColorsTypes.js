ef60cedbeb4f88017018b1f96811eb36d9cf6c1f
eakarr <eaakar.55@gmail.com>
2022-06-14 23:02:00 +0300

    Initial commit

---
+++ src/helpers/pencilColorsTypes.js
@@ -0,0 +1,30 @@
+export const pencilColorsTypes = [
+  {
+    id: "black",
+    colorType: "black",
+  },
+  {
+    id: "red",
+    colorType: "red",
+  },
+  {
+    id: "blue",
+    colorType: "blue",
+  },
+  {
+    id: "green",
+    colorType: "green",
+  },
+  {
+    id: "purple",
+    colorType: "purple",
+  },
+  {
+    id: "yellow",
+    colorType: "yellow",
+  },
+  {
+    id: "gainsboro",
+    colorType: "gainsboro",
+  },
+];
