39c42943c50930fa61d3ae6f5bd1102c1c5a8eb5
eakarr <eaakar.55@gmail.com>
2022-06-15 00:31:56 +0300

    Initial pencil states values changed

--- src/context/StyleOptionsContext.jsx
+++ src/context/StyleOptionsContext.jsx
@@ -3,12 +3,12 @@ import { useState, createContext } from "react";
 export const StyleOptionsContext = createContext();
 
 export const StyleOptionsProvider = ({ children }) => {
-  const [pencilSize, setPencilSize] = useState(0.16);
-  const [pencilThinning, setPencilThinning] = useState(0.5);
-  const [pencilStreamline, setPencilStreamline] = useState(0.5);
-  const [pencilSmoothing, setPencilSmoothing] = useState(0.5);
-  const [pencilTaperStart, setPencilTaperStart] = useState(0.5);
-  const [pencilTaperEnd, setPencilTaperEnd] = useState(0.5);
+  const [pencilSize, setPencilSize] = useState(16);
+  const [pencilThinning, setPencilThinning] = useState(50);
+  const [pencilStreamline, setPencilStreamline] = useState(50);
+  const [pencilSmoothing, setPencilSmoothing] = useState(50);
+  const [pencilTaperStart, setPencilTaperStart] = useState(50);
+  const [pencilTaperEnd, setPencilTaperEnd] = useState(50);
   const [pencilColor, setPencilColor] = useState("black");
 
   return (
