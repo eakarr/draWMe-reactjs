555946cbfc4758afa7c4f0a96f664fbff8382225
eakarr <eaakar.55@gmail.com>
2022-06-16 20:55:00 +0300

    Initial commit

---
+++ src/helpers/rectangleColorsTypes.js
@@ -0,0 +1,34 @@
+export const rectangleColorsTypes = [
+  {
+    id: "transparent",
+    colorType: "transparent",
+  },
+  {
+    id: "black",
+    colorType: "black",
+  },
+  {
+    id: "red",
+    colorType: "red",
+  },
+  {
+    id: "blue",
+    colorType: "blue",
+  },
+  {
+    id: "green",
+    colorType: "green",
+  },
+  {
+    id: "purple",
+    colorType: "purple",
+  },
+  {
+    id: "yellow",
+    colorType: "yellow",
+  },
+  {
+    id: "gainsboro",
+    colorType: "gainsboro",
+  },
+];
