35d28e180d2b84b4702f7ef30d0f3ad8bb7a0d47
eakarr <eaakar.55@gmail.com>
2022-06-18 23:26:13 +0300

    Rectangle and line styles added element generator

--- src/helpers/createElement.js
+++ src/helpers/createElement.js
@@ -2,14 +2,29 @@ import rough from "roughjs/bundled/rough.esm";
 
 const elementGenerator = rough.generator();
 
-const createElement = (id, x1, y1, x2, y2, type) => {
+const createElement = (
+  id,
+  x1,
+  y1,
+  x2,
+  y2,
+  type,
+  lineStyleOptions,
+  rectangleStyleOptions
+) => {
   switch (type) {
     case "line":
     case "rectangle":
       const roughElement =
         type === "line"
-          ? elementGenerator.line(x1, y1, x2, y2)
-          : elementGenerator.rectangle(x1, y1, x2 - x1, y2 - y1);
+          ? elementGenerator.line(x1, y1, x2, y2, lineStyleOptions)
+          : elementGenerator.rectangle(
+              x1,
+              y1,
+              x2 - x1,
+              y2 - y1,
+              rectangleStyleOptions
+            );
       return { id, x1, y1, x2, y2, type, roughElement };
     case "pencil":
       return { id, type, points: [{ x: x1, y: y1 }] };
